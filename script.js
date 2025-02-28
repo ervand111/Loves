dab746bc7b1a0102e34d892588e42c7aa55566ff
Ervand <vip.ervand.88@gmail.com>
2025-02-28 18:26:01 +0400

    change media code

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function () {
             document.body.appendChild(heart);
             heart.style.left = `${Math.random() * 100}%`;
             heart.style.animationDuration = `${3 + Math.random() * 3}s`;
-            
+
             setTimeout(() => {
                 heart.remove();
             }, 5000);
